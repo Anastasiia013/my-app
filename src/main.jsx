7343564f8ff24abb72ccc9c4bd3ce269f716b0aa
ANASTASIIA <anastasiaposokhova@gmail.com>
2025-02-24 20:49:17 +0100

    add components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import App from './components/App/App.jsx'
+import VideoComponent from './components/VideoComponent.jsx'
+import ImageComponent from './components/ImageComponent.jsx'
+import ParagraphsComponent from './components/ParagraphsComponent.jsx'
+import ListsComponent from './components/ListsComponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <VideoComponent />
+    <ImageComponent />
+    <ParagraphsComponent />
+    <ListsComponent />
   </StrictMode>,
 )
